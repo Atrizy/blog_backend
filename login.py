296814c7f79037c837cb2aaec449620fee4588b2
Atrizy <evankeeping21@gmail.com>
2022-03-11 00:06:39 -0330

    Finished the comments in the login.py moving onto comments in other .py files

--- login.py
+++ login.py
@@ -40,14 +40,19 @@ def login_user(email, password):
     cursor.execute("SELECT salt FROM users WHERE email=?", [email])
     ## You only need to fetch one because with user input on the email the api only needs to compare one email to the salt
     salt = cursor.fetchone()
-    ## This is the code thatis executing the 
+    ## This is the code thatis executing the login token creation
     login_token = create_login_token()
+    ## This is comparing the salt to the password
     password = salt[0] + password
+    ## This is comparing the password that has been input to the hashed version of the password
     password = hashlib.sha512(password.encode()).hexdigest()
+    ## This is getting all of the users information
     cursor.execute("SELECT id, email, username, bio, dob, pfp, profile_banner FROM users WHERE email=? AND password=?" [email, password])
+    ## This is the actually compiling all the user info into a single object
     user = cursor.fetchone()
     try:
         if(cursor.rowcount == 1):
+            ## This is what will happen when your login is actually successful your login token and user id will be inserted into the user_session table 
             cursor.execute("INSERT INTO user_session(user_id, login_token) VALUES(?,?)", [user[0], login_token])
             success = True
             id = cursor.lastrowid
@@ -65,8 +70,11 @@ def delete_user(login_token):
     success = False
     conn, cursor = dbi.connect_db()
     try:
+        ## This will check your login_token and see your user credentials
         cursor.execute("SELECT user_id FROM user_session WHERE login_token=?", [login_token])
+        ## This acutally fetches everything from your login token
         user = cursor.fetchone()
+        ## This will delete your user from the db
         cursor.execute("DELETE FROM users WHERE id=?", [user[0]] )
         conn.commit()
         if(cursor.rowcount == 1):
